36b1b4ae5a0c7fd8e44733bd6903a11c514a8742
chynnalew <chynnalew@yahoo.com>
2021-09-28 13:28:18 -0700

    pass tests to read parcel properties

--- Parcels.Tests/ModelTests/ParcelsTests.cs
+++ Parcels.Tests/ModelTests/ParcelsTests.cs
@@ -9,9 +9,32 @@ namespace Parcels.Tests
     [TestMethod]
     public void Parcel_CreateNewInstanceOfParcel_True()
     {
-      Parcel newParcel = new Parcel();
+      Parcel newParcel = new Parcel(1, 1, 1, 1);
       Assert.AreEqual(typeof(Parcel), newParcel.GetType());
     }
-
+    [TestMethod]
+    public void Parcel_ReadsParcelHeight_True()
+    {
+      Parcel testParcel = new Parcel(2, 3, 4, 5);
+      Assert.AreEqual(2, testParcel.Height);
+    }
+    [TestMethod]
+    public void Parcel_ReadsParcelWidth_True()
+    {
+      Parcel testParcel = new Parcel(2, 3, 4, 5);
+      Assert.AreEqual(3, testParcel.Width);
+    }
+    [TestMethod]
+    public void Parcel_ReadsParcelLength_True()
+    {
+      Parcel testParcel = new Parcel(2, 3, 4, 5);
+      Assert.AreEqual(4, testParcel.Length);
+    }
+    [TestMethod]
+    public void Parcel_ReadsParcelWeight_True()
+    {
+      Parcel testParcel = new Parcel(2, 3, 4, 5);
+      Assert.AreEqual(5, testParcel.Weight);
+    }
   }
 }
--- Parcels/Models/Parcels.cs
+++ Parcels/Models/Parcels.cs
@@ -2,9 +2,17 @@ namespace Parcels.Models
 {
   public class Parcel
   {
-    public Parcel()
+    public int Height {get; set;}
+    public int Width {get; set;}
+    public int Length {get; set;}
+    public int Weight {get; set;}
+
+    public Parcel(int height, int width, int length,int weight)
     {
-      
+      Height = height;
+      Width = width;
+      Length = length;
+      Weight = weight;
     }
   }
 }
