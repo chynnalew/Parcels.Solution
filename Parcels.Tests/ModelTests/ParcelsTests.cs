6a32be23dabd8e9b53b483ceff42c22916979aa9
chynnalew <chynnalew@yahoo.com>
2021-09-28 14:14:31 -0700

    pass tests to calculate CostToShip

--- Parcels.Tests/ModelTests/ParcelsTests.cs
+++ Parcels.Tests/ModelTests/ParcelsTests.cs
@@ -9,38 +9,51 @@ namespace Parcels.Tests
     [TestMethod]
     public void Parcel_CreateNewInstanceOfParcel_True()
     {
-      Parcel newParcel = new Parcel(1, 1, 1, 1);
+      Parcel newParcel = new Parcel(1, 1, 1);
       Assert.AreEqual(typeof(Parcel), newParcel.GetType());
     }
     [TestMethod]
     public void Parcel_ReadsParcelHeight_True()
     {
-      Parcel testParcel = new Parcel(2, 3, 4, 5);
+      Parcel testParcel = new Parcel(2, 3, 4);
       Assert.AreEqual(2, testParcel.Height);
     }
     [TestMethod]
     public void Parcel_ReadsParcelWidth_True()
     {
-      Parcel testParcel = new Parcel(2, 3, 4, 5);
+      Parcel testParcel = new Parcel(2, 3, 4);
       Assert.AreEqual(3, testParcel.Width);
     }
     [TestMethod]
     public void Parcel_ReadsParcelLength_True()
     {
-      Parcel testParcel = new Parcel(2, 3, 4, 5);
+      Parcel testParcel = new Parcel(2, 3, 4);
       Assert.AreEqual(4, testParcel.Length);
-    }
-    [TestMethod]
-    public void Parcel_ReadsParcelWeight_True()
-    {
-      Parcel testParcel = new Parcel(2, 3, 4, 5);
-      Assert.AreEqual(5, testParcel.Weight);
     }
     [TestMethod]
     public void Volume_ReturnsVolumeOfTestParcel_True()
     {
-      Parcel testParcel = new Parcel(2, 3, 4, 5);
+      Parcel testParcel = new Parcel(2, 3, 4);
       Assert.AreEqual(24, testParcel.Volume());
     }
+    [TestMethod]
+    public void CostToShip_ReturnsCostOfSmallPackage_True()
+    {
+      Parcel testParcel = new Parcel(1, 1, 1);
+      Assert.AreEqual(0.25, testParcel.CostToShip());
+    }
+    [TestMethod]
+    public void CostToShip_ReturnsCostOfMediumPackage_True()
+    {
+      Parcel testParcel = new Parcel(10, 1, 5);
+      Assert.AreEqual(12.5, testParcel.CostToShip());
+    }
+    [TestMethod]
+    public void CostToShip_ReturnsCostOfLargePackage_True()
+    {
+      Parcel testParcel = new Parcel(10, 9, 2);
+      Assert.AreEqual(45, testParcel.CostToShip());
+    }
+    
   }
 }
--- Parcels/Models/Parcels.cs
+++ Parcels/Models/Parcels.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace Parcels.Models
 {
   public class Parcel
@@ -5,19 +8,23 @@ namespace Parcels.Models
     public int Height {get; set;}
     public int Width {get; set;}
     public int Length {get; set;}
-    public int Weight {get; set;}
 
-    public Parcel(int height, int width, int length,int weight)
+    public Parcel(int height, int width, int length)
     {
       Height = height;
       Width = width;
       Length = length;
-      Weight = weight;
     }
     public int Volume()
     {
       int volume = this.Height * this.Width * this.Length;
       return volume;
+    }
+    public double CostToShip()
+    {
+      double dubVol = (double)this.Volume();
+      double cost = dubVol*0.25;
+      return cost;
     }
   }
 }
